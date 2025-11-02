cd86df6e502b3622cf00bcbbc3c7db0d2627b229
Zorawar Purohit <zorawar@m37labs.com>
2025-11-03 00:03:17 +0530

    first commit - fast api lesson 1

---
+++ README.md
@@ -0,0 +1,29 @@
+# FastAPI - Tinker and Learn Features:
+
+1. Parameter selection using Enums
+2. Path and query parameters
+3. Conditional logic based on query parameters
+
+# Steps to run the application:
+
+
+1. Clone this repository and install the required packages:
+    `pip install -r requirements.txt`
+
+
+
+2. Save this code in a file named `main.py`
+
+3. Run the application using Uvicorn:
+   `uvicorn main:app --reload`  or  `uvicorn main:app --host 0.0.0.0 --port 8000 --reload`
+
+4. Access the application in your browser at:
+   `http://localhost:8000`
+
+
+> Create virtual environment before step 1 (optional but recommended):
+
+   `python -m venv venv`
+
+   - `source venv/bin/activate` -> on macOS/Linux use
+   - `venv\Scripts\activate` -> on Windows use 
---
+++ main.py
@@ -0,0 +1,51 @@
+from enum import Enum
+from fastapi import FastAPI
+import datetime
+
+app = FastAPI()
+
+# To run the application, use the command: `uvicorn main:app --reload`
+
+# Define an enumeration for model names to restrict valid input values in path parameters
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+# Define a route that uses the ModelName enumeration as a path parameter
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Getting the hands on AlexNet"}
+    elif model_name.value == "resnet":
+        return {"model_name": model_name, "message": "Getting the ResNet modelz"}
+    return {"model_name": model_name, "message": "Getting the LeNet model"}
+
+
+# Define a route that calculates future age based on current age and a fixed number of years
+# Notice how any parameter not defined in the path is treated as a query parameter
+
+# @app.get("/age/{age}")
+# async def calculate_age(age: int, years: int = 50):
+#     future_age = age + years
+#     current_year = datetime.datetime.now().year
+#     future_year = current_year + years
+#     return {"current_age": age, "future_age": future_age, "year": future_year}
+
+
+
+
+@app.get("/age/{age}")
+async def calculate_age(age: int, years: int = 50, desired: int | None = None, activate: bool = True):
+    future_age = age + years
+    current_year = datetime.datetime.now().year
+    future_year = current_year + years
+    if desired is not None and activate:
+        years_needed = desired - age
+        if years_needed > 0:
+            future_age = desired
+            future_year = current_year + years_needed
+    return {"current_age": age, "future_age": future_age, "year": future_year}
