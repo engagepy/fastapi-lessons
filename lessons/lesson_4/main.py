410d425d70be210deec68af8509fbe2e1d916a83
Zorawar Purohit <zorawar@m37labs.com>
2026-01-13 10:49:20 +0530

    tag update for lesson clarity

--- lessons/lesson_4/main.py
+++ lessons/lesson_4/main.py
@@ -9,7 +9,7 @@ class Item(BaseModel):
 
 app = FastAPI(title="Lesson 4 - Request Body Parameters with Pydantic", version="1.0.0", description="An example FastAPI application demonstrating body + path + query parameters")
 
-@app.put("/items/{item_id}", tags=["Observe request body parameters using Pydantic Model along with path and query parameters"])
+@app.put("/items/{item_id}", tags=["Observe request body parameters using Pydantic Model + path + query parameters"])
 async def update_item(item_id: int, item: Item, q: str | None = None):
     result = {"item_id": item_id, "item": item}
     if q:
