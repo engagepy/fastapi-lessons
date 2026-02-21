e1e52e9a4d286560cb0e3e7d387a8d0ed3a159d2
Zorawar Purohit <zorawar@m37labs.com>
2026-02-22 03:00:55 +0530

    using deeply nested type params in Body models using pydantic

--- lessons/lesson_15/router.py
+++ lessons/lesson_15/router.py
@@ -10,3 +10,24 @@ router = APIRouter(
 )
 
 
+class Item(BaseModel):
+    name : str = Field(default="Sample Item", example="Book", max_length=100)
+    price : float = Field(default=0.00, example=19.99, ge=0)
+    description : str | None = Field(default="A sample item for demonstration", example="A great book about FastAPI", max_length=300)  
+    tax : float = Field(default=0.0, example=1.99, ge=0)
+    tags : list[str] = Field(default=["sample", "item"], example=["book", "fastapi", "python"]) 
+
+@router.post(
+        "/items/{item_id}",
+        summary="Create an Item with Path and Body Parameters",
+        description="Demonstrates how to use deeply nested Pydantic models in the request body while also accepting path parameters for creating an item",
+        )
+async def create_item(
+    item_id: Annotated[int, Path(title="Item ID", description="The ID of the item to create", gt=0)],
+    item: Annotated[Item, Body(title="Item", description="The details of the item to create")]
+):
+    return {
+        "item_id": item_id,
+        "item": item.model_dump()
+    }
+
